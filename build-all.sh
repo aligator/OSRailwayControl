7892fae2c87be9083c7f242b5bb18103d3984033
aligator <aligator@suncraft-server.de>
2020-08-09 15:09:24 +0200

    add build script & circle-ci config to automatically generate releases

---
+++ build-all.sh
@@ -0,0 +1,22 @@
+# /bin/sh
+mkdir build
+
+go generate
+
+echo "Building darwin"
+GOOS=darwin GOARCH=amd64 go build
+mv OSRailwayControl build/OSRailwayControl_macOS_amd64
+
+echo "Building Linux"
+GOOS=linux GOARCH=amd64 go build
+mv OSRailwayControl build/OSRailwayControl_linux_amd64
+GOOS=linux GOARCH=arm go build
+mv OSRailwayControl build/OSRailwayControl_linux_arm
+GOOS=linux GOARCH=arm64 go build
+mv OSRailwayControl build/OSRailwayControl_linux_arm64
+
+echo "Building windows"
+GOOS=windows GOARCH=amd64 go build
+mv OSRailwayControl.exe build/OSRailwayControl_windows_amd64.exe
+
+echo "Completed"
